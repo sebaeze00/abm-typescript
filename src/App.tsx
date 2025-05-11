3c074f8cf39708996dd419df5b524d8d66c2e949
seba <sebaeze00@gmail.com>
2025-05-11 16:50:00 -0300

    Se creó 2 inputs y se le pasa el valor para agregar y editar usuarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+type User = {
+  id: number
+  name: string
+  email: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState<User[]>([])
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [editingId, setEditingId] = useState<number | null>(null)
+
+  const addOrUpdate = () => {
+
+    if (!name || !email) {
+      return
+    }
+
+    if (editingId !== null) {
+      setUser(prev =>
+        prev.map(user => user.id === editingId ? { ...user, name, email } : user)
+      )
+      setEditingId(null)
+
+    } else {
+      const newUser: User = {
+        id: Date.now(),
+        name,
+        email,
+      }
+      setUser(prev => [...prev, newUser])
+    }
+    setName('')
+    setEmail('')
+  }
+
+  const editUser = (user: User) => {
+    setName(user.name)
+    setEmail(user.email)
+    setEditingId(user.id)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div >
+        <input
+          type="text"
+          placeholder="Nombre"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <button
+          onClick={addOrUpdate}
+        >
+          {editingId ? 'Actualizar' : 'Agregar'}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <ul>{user.map(u => (
+        <li key={u.id}>
+          <div>
+            <p >{u.name}</p>
+            <p >{u.email}</p>
+          </div>
+
+          <button
+            onClick={() => editUser(u)}
+            className="text-blue-500 hover:underline"
+          >
+            Editar
+          </button>
+
+        </li>
+      ))}</ul>
     </>
   )
 }
